3911baf259e214bb8e6e532e2d95a9c10bccdc7a
parkab27 <pkabaria77@gmail.com>
2024-11-02 14:05:44 -0400

    basic bottom bar layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,41 @@
+import React from 'react';
 import './App.css';
-import logo from './logo.svg';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload. BBBBBBBBB
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. BBBBBBBBB
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="MainContent">
+        {}
+      </div>
+      <div className="BottomBar">
+      <div className="Circle HoldItem">Hold</div>
+        <button className="Circle Stopwatch">Stopwatch</button>
+        <input type="text" className="SearchBar" placeholder="Search..." />
+        <div className="Circle LargeCircle">3</div>
+        <div className="Circle MediumCircle">2</div>
+        <div className="Circle SmallCircle">1</div>
+      </div>
     </div>
   );
 }
