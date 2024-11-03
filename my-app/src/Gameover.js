915f12899ac1d4f62cf138bfb65b10d8224ad601
parkab27 <pkabaria77@gmail.com>
2024-11-03 11:30:11 -0500

    added timer and restart fixes

--- my-app/src/Gameover.js
+++ my-app/src/Gameover.js
@@ -1,11 +1,12 @@
 // GameOver.js
 import React from 'react';
 
-const GameOver = ({ onRestart, onReturnToTitle }) => {
+const GameOver = ({ onRestart, onReturnToTitle, score }) => {
   return (
     <div className="GameOverScreen">
       <h1>Game Over</h1>
       <p>Time is up! The Lexicogs ran out of steam.</p>
+      <p>Your score: {score}</p>
       <button onClick={onRestart}>Play Again</button>
       <button onClick={onReturnToTitle}>Return to Title</button>
     </div>
